c0962c072f5d45c51c9ed3e0f0d1aa53c0509db2
izhre <izhrenat@mail.ru>
2021-10-14 12:34:33 +0300

    add: combined steps

--- TicTac/TicTac.py
+++ TicTac/TicTac.py
@@ -1,9 +1,8 @@
 class Field:
 
     def __init__(self, size=3):
-        self.size = size
         self.count = size ** 2
-        self.steps = 0
+        self.player = False
         self.end = False
         self.cells = [' ' for _ in range(self.count)]
 
@@ -28,15 +27,12 @@ class Field:
                 return
         a.check_tie()
 
-    def stepX(self, x):
+    def step(self, x):
         if self.is_cell_empty(x):
-            self.cells[x - 1] = 'X'
-            a.steps += 1
-
-    def stepO(self, x):
-        if self.is_cell_empty(x):
-            self.cells[x - 1] = 'O'
-            a.steps += 1
+            self.cells[x - 1] = 'O' if self.player else 'X'
+            a.player = not a.player
+        else:
+            print('This cell is busy, try another :)')
 
     def print(self):
         print(self.cells[0] + '|' + self.cells[1] + '|' + self.cells[2])
@@ -45,15 +41,14 @@ class Field:
         print('-' * 5)
         print(self.cells[6] + '|' + self.cells[7] + '|' + self.cells[8])
 
+    def game(self):
+        a.print()
+        while not a.end:
+            x = int(input('Player ' + ('2' if self.player else '1') + ', make ur choice '))
+            self.step(x)
+            a.print()
+            a.check_win()
+
 
 a = Field()
-a.print()
-while not a.end:
-    if a.steps % 2 == 0:
-        x = int(input('Player 1, Make ur choice: '))
-        a.stepX(x)
-    else:
-        x = int(input('Player 2, Make ur choice: '))
-        a.stepO(x)
-    a.print()
-    a.check_win()
+a.game()
