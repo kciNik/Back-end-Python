7efeeaf94dc1863fe8aaf39cfb2d80e71a9a805b
izhre <izhrenat@mail.ru>
2021-10-18 12:40:38 +0300

    add: exceptions

--- TicTac/TicTac.py
+++ TicTac/TicTac.py
@@ -1,38 +1,59 @@
+class WrongInputException(Exception):
+    """
+    Exception for wrong digit input
+    """
+
+
+class NotDigitInputException(Exception):
+    """
+    Exception for non digit input
+    """
+
+
+class BusyCellException(Exception):
+    """
+    Exception for busy cell
+    """
+
+
+class WinException(Exception):
+    """
+    Exception for win
+    """
+
+
 class Field:
 
-    def __init__(self, size=3):
-        self.count = size ** 2
+    def __init__(self):
         self.player = False
-        self.end = False
-        self.cells = [' ' for _ in range(self.count)]
-
-    def is_cell_empty(self, x):
-        return self.cells[x - 1] == ' '
+        self.cells = [' ' for _ in range(9)]
 
     def check_tie(self):
-        check = 0
-        for i in self.cells:
-            if i != ' ':
-                check += 1
-        if check == self.count:
-            print('GG')
-            self.end = True
+        if self.cells.count(' ') == 0:
+            return True
+        return False
 
     def check_win(self):
         win_comb = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
         for each in win_comb:
             if self.cells[each[0]] == self.cells[each[1]] == self.cells[each[2]] != ' ':
-                print(self.cells[each[0]] + ' win!')
-                self.end = True
-                return
-        a.check_tie()
+                raise WinException
 
     def step(self, x):
-        if self.is_cell_empty(x):
-            self.cells[x - 1] = 'O' if self.player else 'X'
-            a.player = not a.player
-        else:
-            print('This cell is busy, try another :)')
+        self.cells[x - 1] = 'O' if self.player else 'X'
+        self.check_win()
+        self.player = not self.player
+
+    def check_input(self):
+        data = input('Player ' + ('2' if self.player else '1') + ', make ur choice -> ')
+        if not data.isdigit():
+            raise NotDigitInputException
+        index = int(data)
+        if index < 1 or index > 9:
+            raise WrongInputException
+        if not self.cells[index - 1] == ' ':
+            raise BusyCellException
+        return index
 
     def print(self):
         print(self.cells[0] + '|' + self.cells[1] + '|' + self.cells[2])
@@ -42,13 +63,29 @@ class Field:
         print(self.cells[6] + '|' + self.cells[7] + '|' + self.cells[8])
 
     def game(self):
-        a.print()
-        while not a.end:
-            x = int(input('Player ' + ('2' if self.player else '1') + ', make ur choice '))
-            self.step(x)
-            a.print()
-            a.check_win()
+        while not self.check_tie():
+            self.print()
+            try:
+                x = self.check_input()
+            except NotDigitInputException:
+                print('Ur input is not a digit, please enter 1 to 9')
+                continue
+            except WrongInputException:
+                print('Ur input is wrong, please enter 1 to 9')
+                continue
+            except BusyCellException:
+                print('This cell is busy, please try another :)')
+                continue
+            try:
+                self.step(int(x))
+            except WinException:
+                print('Player ' + ('2' if self.player else '1') + ' wins!')
+                self.print()
+                return
+        print('Draw!')
+        self.print()
 
 
-a = Field()
-a.game()
+if __name__ == '__main__':
+    a = Field()
+    a.game()
